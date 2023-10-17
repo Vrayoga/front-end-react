3b28df7df2ccfb9a58df7be617c3d1a3448df814
Vrayoga <vrayogal227@gmail.com>
2023-10-17 16:00:39 +0700

    react tugas 19

---
+++ src/pages/mahasiswa.js
@@ -0,0 +1,14 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+
+function mahasiswa(){
+    return(
+        <Container>
+            <row>
+                <Col> <h2> data mahasiswa</h2> </Col>
+            </row>
+        </Container>
+    )
+}
+
+export default mahasiswa;
